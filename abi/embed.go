47c9614cfba32417831fa0617f56e51f055d22bc
yinwei <82604458@qq.com>
2025-10-22 21:39:05 +0800

    feat：统一abi获取方式

--- abi/embed.go
+++ abi/embed.go
@@ -9,3 +9,14 @@ var EnterpriseWallet []byte
 
 //go:embed EnterpriseWalletFactory.json
 var EnterpriseWalletFactory []byte
+
+//go:embed PaymentAccount.json
+var PaymentAccount []byte
+
+// Safe ABI definitions embedded at compile time.
+
+//go:embed Safe.json
+var Safe []byte
+
+//go:embed SafeProxyFactory_full.json
+var SafeProxyFactory []byte
